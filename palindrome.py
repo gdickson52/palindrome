13df12b82b115b424ac08d898bf0babdc75fab58
gdickson52 <gdickson52@gmail.com>
2019-06-04 16:46:29 -0400

    palindrome

---
+++ palindrome.py
@@ -0,0 +1,29 @@
+# program to check if a string
+#   is palindrome or not
+
+# change this value for a different output
+my_str = "Stunt Nuts"
+all_letters = "abcdefghijklmnopqrstuvwxyz"
+found_letters = []
+for letter in my_str.lower():
+    if letter in all_letters: 
+        found_letters.append(letter)
+        print(found_letters)
+
+# make it suitable for caseless comparison
+
+
+
+# reverse the string
+rev_str = reversed(found_letters)
+
+# check if the string is equal to its reverse
+print(found_letters)
+if list(found_letters) == list(rev_str):
+        print("it is palindrome")
+else:
+    print("it is not a palindrome")
+
+
+
+
